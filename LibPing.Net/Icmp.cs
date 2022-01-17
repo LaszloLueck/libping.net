4d76283eeee5ad071d8e3574b6d09466950722e3
Laszlo Lueck <llueck@gmail.com>
2022-01-17 21:21:26 +0100

    add more comments to methods and return object

--- LibPing.Net/Icmp.cs
+++ LibPing.Net/Icmp.cs
@@ -2,9 +2,26 @@
 
 public class Icmp
 {
+    /// <summary>
+    /// process an ICMP echo (RFC-792) command, also called ping. It does the job with both IPv4 and IPv6.
+    /// </summary>
+    /// <param name="ipOrHostName">the IP-address or the Hostname to ping to. If you pass an IPv4 address, IPv4 echo is also used. If you pass an IPv6 address, then according to IPv6 rules. If you enter a hostname, it will be checked if the hostname could be resolved as an IPv6 address, if not, IPv4 will be used. </param>
+    /// <param name="ttl">The meaning of ttl (time to live) is not so much a time but how many hops (endpoints) may be spent on the ping. E.g. the endpoint to be pinged is 8 hops away. If you enter a 3 as ttl, a result is returned from the 3rd hop in the query series with a ResultType for IPv4 = 11 / IPv6 = 3 (means Time exceeded). ttl is especially useful for traceroute queries. </param>
+    /// <param name="receiveTimeout">time in milliseconds. After this time, the connection would be unsuccessful closed with an exception.</param>
+    /// <param name="cancellationToken">cancellation token for async requests we made.</param>
+    /// <returns>returns a IcmpResponse object, or in case of an error, the exception</returns>
     public static async Task<IcmpResponse> Ping(string ipOrHostName, int ttl, int receiveTimeout,
         CancellationToken cancellationToken) => await Ping(ipOrHostName, ttl, receiveTimeout, true, cancellationToken);
     
+    /// <summary>
+    /// process an ICMP echo (RFC-792) command, also called ping. It does the job with both IPv4 and IPv6.
+    /// </summary>
+    /// <param name="ipOrHostName">the IP-address or the Hostname to ping to. If you pass an IPv4 address, IPv4 echo is also used. If you pass an IPv6 address, then according to IPv6 rules. If you enter a hostname, it will be checked if the hostname could be resolved as an IPv6 address, if not, IPv4 will be used. </param>
+    /// <param name="ttl">The meaning of ttl (time to live) is not so much a time but how many hops (endpoints) may be spent on the ping. E.g. the endpoint to be pinged is 8 hops away. If you enter a 3 as ttl, a result is returned from the 3rd hop in the query series with a ResultType for IPv4 = 11 / IPv6 = 3 (means Time exceeded). ttl is especially useful for traceroute queries. </param>
+    /// <param name="receiveTimeout">time in milliseconds. After this time, the connection would be unsuccessful closed with an exception.</param>
+    /// <param name="dontFragment">split the datagram to multiple frames when the payload to send is bigger than the the framesize (mtu). It does not work on apple macs.</param>
+    /// <param name="cancellationToken">cancellation token for async requests we made.</param>
+    /// <returns>returns a IcmpResponse object, or in case of an error, the exception</returns>
     public static async Task<IcmpResponse> Ping(string ipOrHostName, int ttl, int receiveTimeout, bool dontFragment, CancellationToken cancellationToken = default)
     {
         var request = new IcmpRequest();
--- LibPing.Net/IcmpResponse.cs
+++ LibPing.Net/IcmpResponse.cs
@@ -2,21 +2,62 @@ using System.Collections.Immutable;
 using System.Net;
 
 namespace LibPing.Net;
-
+/// <summary>
+/// return object from ping. It delivers a lot more information as the standard operating system ping command. 
+/// </summary>
 public class IcmpResponse
 {
+    /// <summary>
+    /// the whole received datagram as byte array 
+    /// </summary>
     public readonly ImmutableArray<byte> Data;
+    /// <summary>
+    /// the returned code from echo reply
+    /// </summary>
     public readonly int Code;
+    /// <summary>
+    /// the returned ttl (only ipv4)
+    /// </summary>
     public readonly int Ttl;
+    /// <summary>
+    /// the checksum of the return
+    /// </summary>
     public readonly int Checksum;
+    /// <summary>
+    /// the return type in as byte value. Attention! IPv4 and IPv6 uses different return values
+    /// </summary>
     public readonly byte Type;
+    /// <summary>
+    /// the payload size
+    /// </summary>
     public readonly int PayloadSize;
+    /// <summary>
+    /// if the ttl is too slow for the amount of hops to the endpoint, the hop on the ttl position answered here. 
+    /// </summary>
     public readonly IPEndPoint Origin;
+    /// <summary>
+    /// the local ip from which the ping is send
+    /// </summary>
     public readonly IPEndPoint? FromIp;
+    /// <summary>
+    /// the identifier from reply
+    /// </summary>
     public readonly int Identifier;
+    /// <summary>
+    /// the sequence number from response
+    /// </summary>
     public readonly int Sequence;
+    /// <summary>
+    /// The addressfamily with which the ping was executed. Either IPv4 or IPv6
+    /// </summary>
     public readonly IpAddressFamily AddressFamily;
+    /// <summary>
+    /// the roundtriptime from send the ping to receive the result in milliseconds
+    /// </summary>
     public readonly long RoundTripTime;
+    /// <summary>
+    /// the response type as readable string
+    /// </summary>
     public readonly string TypeString;
 
     internal IcmpResponse(IPEndPoint origin, int payloadSize, byte[] data, IPEndPoint? fromIp,
