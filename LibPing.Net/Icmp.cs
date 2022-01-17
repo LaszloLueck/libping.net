5efcb9f25030b37048b91bdb2ba9770d816f67ab
Laszlo Lueck <llueck@gmail.com>
2022-01-17 19:42:24 +0100

    method overload for fragmentation

--- LibPing.Net/Icmp.cs
+++ LibPing.Net/Icmp.cs
@@ -2,7 +2,10 @@
 
 public class Icmp
 {
-    public static async Task<IcmpResponse> Ping(string ipOrHostName, int ttl, int receiveTimeout, bool dontFragment = true, CancellationToken cancellationToken = default)
+    public static async Task<IcmpResponse> Ping(string ipOrHostName, int ttl, int receiveTimeout,
+        CancellationToken cancellationToken) => await Ping(ipOrHostName, ttl, receiveTimeout, true, cancellationToken);
+    
+    public static async Task<IcmpResponse> Ping(string ipOrHostName, int ttl, int receiveTimeout, bool dontFragment, CancellationToken cancellationToken = default)
     {
         var request = new IcmpRequest();
         await request.Init(ttl, ipOrHostName, receiveTimeout, dontFragment, cancellationToken);
