b4c9ca6a13445cad4432e6bf755c75e17e1fa14d
Laszlo Lueck <llueck@gmail.com>
2022-02-03 17:14:53 +0100

    init commit

---
+++ LibPing.Net/Traceroute.cs
@@ -0,0 +1,13 @@
+﻿namespace LibPing.Net;
+
+public static class Traceroute
+{
+
+    public static async Task<IEnumerable<IcmpResponse>> DoTraceroute(string ipAddressOrHost, int maxHops,
+        int receiveTimeout, CancellationToken cancellationToken)
+    {
+        
+    }
+
+
+}
