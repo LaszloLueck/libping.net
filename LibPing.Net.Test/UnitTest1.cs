34ad9979dafbd382c721eccf5e95f0e891ea4c12
Laszlo Lueck <llueck@gmail.com>
2022-05-03 19:39:12 +0200

    add testproject with faketest

---
+++ LibPing.Net.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+using Xunit;
+
+namespace LibPing.Net.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        true.Should().BeTrue();
+    }
+}
